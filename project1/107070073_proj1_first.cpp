c8e90e4f68345802a7e66143f0ab8cfe48e52a08
whitepig3s <whitepig3s@gmail.com>
2020-10-11 00:19:31 +0800

    C++ 為什麼能不像Python一樣內建split()

--- project1/107070073_proj1_first.cpp
+++ project1/107070073_proj1_first.cpp
@@ -5,24 +5,40 @@
 #include <sstream>
 using namespace std;
 
+vector<string> split(string str)
+{
+    istringstream in(str);
+    string tmp_str;
+    vector<string> vec;
+    while (in >> tmp_str) 
+        vec.push_back(tmp_str);
+    
+    return vec;
+}
+
 int main(int argc, char *argv[])
 {
     ifstream file(argv[1], ios::in);
     int m /*column*/, n /*row*/;
     file >> m >> n;
     cout << "column:" << m << endl
-         << "rol:" << n;
-
-    int a, b;
+         << "rol:" << n<<endl;
+    
     string line;
+    getline(file, line); //要把換行吃掉
     do
     {
+        int point,move;
         getline(file, line);
         if (line != "End")
         {
-            
+            vector<string> v;
+            v = split(line);
+            point=stoi(v[1]);
+            move=stoi(v[2]);
+            cout<<v[0]<<" "<<point<<" "<<move<<endl;
         }
-    } while (!file.eof());
+    }while (!file.eof());
 
     file.close();
     return 0;
