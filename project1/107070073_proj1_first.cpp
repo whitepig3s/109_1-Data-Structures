08eac06e7d6a4e8b1d51e920cf1880036a0b065a
whitepig3s <whitepig3s@gmail.com>
2020-10-10 23:47:55 +0800

    處理字串真的很麻煩

--- project1/107070073_proj1_first.cpp
+++ project1/107070073_proj1_first.cpp
@@ -1,9 +1,29 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <sstream>
 using namespace std;
 
-int main (){
+int main(int argc, char *argv[])
+{
+    ifstream file(argv[1], ios::in);
+    int m /*column*/, n /*row*/;
+    file >> m >> n;
+    cout << "column:" << m << endl
+         << "rol:" << n;
 
+    int a, b;
+    string line;
+    do
+    {
+        getline(file, line);
+        if (line != "End")
+        {
+            
+        }
+    } while (!file.eof());
 
-
+    file.close();
     return 0;
 }
