521f3a7fb0075f5716cea1af611a9de20a13d5b5
whitepig3s <whitepig3s@gmail.com>
2020-10-12 23:21:15 +0800

    block 完成

--- project1/107070073_proj1_first.cpp
+++ project1/107070073_proj1_first.cpp
@@ -16,6 +16,63 @@ vector<string> split(string str)
     return vec;
 }
 
+//blocks
+bool T1[2][3]{
+    0, 1, 0,
+    1, 1, 1};
+bool T2[3][2]{
+    0, 1,
+    1, 1,
+    0, 1};
+bool T3[2][3]{
+    1, 1, 1,
+    0, 1, 0};
+bool T4[3][2]{
+    1, 0,
+    1, 1,
+    1, 0};
+bool L1[3][2]{
+    1, 1,
+    1, 0,
+    1, 0};
+bool L2[2][3]{
+    1, 0, 0,
+    1, 1, 1};
+bool L3[3][2]{
+    1, 0,
+    1, 0,
+    1, 1};
+bool L4[2][3]{
+    1, 1, 1,
+    0, 0, 1};
+bool J1[3][2]{
+    1, 1,
+    0, 1,
+    0, 1};
+bool J2[2][3]{
+    1, 1, 1,
+    1, 0, 0};
+bool J3[3][2]{
+    1, 0,
+    1, 0,
+    1, 1};
+bool J4[2][3]{
+    0, 0, 1,
+    1, 1, 1};
+bool I1[4][1]{
+    1,
+    1,
+    1,
+    1};
+bool I2[1][4]{
+    1,
+    1,
+    1,
+    1};
+bool O[2][2]{
+    1, 1,
+    1, 1};
+
 int main(int argc, char *argv[])
 {
     ifstream file(argv[1], ios::in);
@@ -24,21 +81,17 @@ int main(int argc, char *argv[])
     /*cout << "row:" << m << endl
          << "column:" << n << endl;*/
 
-    /*bool **table = new bool *[m];  
-    for (int i = 0; i < m; ++i)
-        table[i] = new bool[n];*/
-
-    int *table = new int[m * n];
+    bool *table = new bool[m * n]; //declare dynamic 2D arr
     for (int i = 0; i < m; i++)
         for (int j = 0; j < n; j++)
-            *(table + i * n + j) = i*10 + j;
+            *(table + i * n + j) = false;
 
-    for (int i = 0; i < m; i++)
+    /*for (int i = 0; i < m; i++)
     {
         for (int j = 0; j < n; j++)
             cout << *(table + i * n + j) << " ";
         cout << endl;
-    }
+    }*/
 
     string line;
     getline(file, line); //要把換行吃掉
@@ -48,7 +101,7 @@ int main(int argc, char *argv[])
         getline(file, line);
         if (line != "End")
         {
-            vector<string> v;
+            vector<string> v; //v[0] block symbol
             v = split(line);
             point = stoi(v[1]);
             move = stoi(v[2]);
