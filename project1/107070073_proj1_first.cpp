94cad21c04f19a1063a2caf323059ec56c4b1784
whitepig3s <whitepig3s@gmail.com>
2020-10-14 08:48:56 +0800

    block 改成 4*4

--- project1/107070073_proj1_first.cpp
+++ project1/107070073_proj1_first.cpp
@@ -16,108 +16,154 @@ vector<string> split(string str)
     return vec;
 }
 
-//blocks
-bool T1_arr[2][3]{
-    0, 1, 0,
-    1, 1, 1};
-bool T2_arr[3][2]{
-    0, 1,
-    1, 1,
-    0, 1};
-bool T3_arr[2][3]{
-    1, 1, 1,
-    0, 1, 0};
-bool T4_arr[3][2]{
-    1, 0,
-    1, 1,
-    1, 0};
-bool L1_arr[3][2]{
-    1, 1,
-    1, 0,
-    1, 0};
-bool L2_arr[2][3]{
-    1, 0, 0,
-    1, 1, 1};
-bool L3_arr[3][2]{
-    1, 0,
-    1, 0,
-    1, 1};
-bool L4_arr[2][3]{
-    1, 1, 1,
-    0, 0, 1};
-bool J1_arr[3][2]{
-    1, 1,
-    0, 1,
-    0, 1};
-bool J2_arr[2][3]{
-    1, 1, 1,
-    1, 0, 0};
-bool J3_arr[3][2]{
-    1, 0,
-    1, 0,
-    1, 1};
-bool J4_arr[2][3]{
-    0, 0, 1,
-    1, 1, 1};
-bool S1_arr[2][3]{
-    1, 1, 0,
-    0, 1, 1};
-bool S2_arr[3][2]{
-    0, 1,
-    1, 1,
-    1, 0};
-bool Z1_arr[2][3]{
-    0, 1, 1,
-    1, 1, 0};
-bool Z2_arr[3][2]{
-    1, 0,
-    1, 1,
-    0, 1};
-bool I1_arr[4][1]{
-    1,
-    1,
-    1,
-    1};
-bool I2_arr[1][4]{
-    1,
-    1,
-    1,
-    1};
-bool O_arr[2][2]{
-    1, 1,
-    1, 1};
+bool table[15][40]{false};
 
-/*enum blocks {
-    T1,T2,T3,T4,
-    L1,L2,L3,L4,
-    J1,J2,J3,J4,
-    S1,S2,
-    Z1,Z2,
-    I1,I2,
-    O
-};*/
+//blocks
+bool T1_arr[4][4]{
+    0, 1, 0, 0,
+    1, 1, 1, 0,
+    0, 0, 0, 0,
+    0, 0, 0, 0};
+bool T2_arr[4][4]{
+    0, 1, 0, 0,
+    1, 1, 0, 0,
+    0, 1, 0, 0,
+    0, 0, 0, 0};
+bool T3_arr[4][4]{
+    1, 1, 1, 0,
+    0, 1, 0, 0,
+    0, 0, 0, 0,
+    0, 0, 0, 0};
+bool T4_arr[4][4]{
+    1, 0, 0, 0,
+    1, 1, 0, 0,
+    1, 0, 0, 0,
+    0, 0, 0, 0};
+bool L1_arr[4][4]{
+    1, 1, 0, 0,
+    1, 0, 0, 0,
+    1, 0, 0, 0,
+    0, 0, 0, 0};
+bool L2_arr[4][4]{
+    1, 0, 0, 0,
+    1, 1, 1, 0,
+    0, 0, 0, 0,
+    0, 0, 0, 0};
+bool L3_arr[4][4]{
+    1, 0, 0, 0,
+    1, 0, 0, 0,
+    1, 1, 0, 0,
+    0, 0, 0, 0};
+bool L4_arr[4][4]{
+    1, 1, 1, 0,
+    0, 0, 1, 0,
+    0, 0, 0, 0,
+    0, 0, 0, 0};
+bool J1_arr[4][4]{
+    1, 1, 0, 0,
+    0, 1, 0, 0,
+    0, 1, 0, 0,
+    0, 0, 0, 0};
+bool J2_arr[4][4]{
+    1, 1, 1, 0,
+    1, 0, 0, 0,
+    0, 0, 0, 0,
+    0, 0, 0, 0};
+bool J3_arr[4][4]{
+    1, 0, 0, 0,
+    1, 0, 0, 0,
+    1, 1, 0, 0,
+    0, 0, 0, 0};
+bool J4_arr[4][4]{
+    0, 0, 1, 0,
+    1, 1, 1, 0,
+    0, 0, 0, 0,
+    0, 0, 0, 0};
+bool S1_arr[4][4]{
+    1, 1, 0, 0,
+    0, 1, 1, 0,
+    0, 0, 0, 0,
+    0, 0, 0, 0};
+bool S2_arr[4][4]{
+    0, 1, 0, 0,
+    1, 1, 0, 0,
+    1, 0, 0, 0,
+    0, 0, 0, 0};
+bool Z1_arr[4][4]{
+    0, 1, 1, 0,
+    1, 1, 0, 0,
+    0, 0, 0, 0,
+    0, 0, 0, 0};
+bool Z2_arr[4][4]{
+    1, 0, 0, 0,
+    1, 1, 0, 0,
+    0, 1, 0, 0,
+    0, 0, 0, 0};
+bool I1_arr[4][4]{
+    1, 0, 0, 0,
+    1, 0, 0, 0,
+    1, 0, 0, 0,
+    1, 0, 0, 0};
+bool I2_arr[4][4]{
+    1, 1, 1, 1,
+    0, 0, 0, 0,
+    0, 0, 0, 0,
+    0, 0, 0, 0};
+bool O_arr[4][4]{
+    1, 1, 0, 0,
+    1, 1, 0, 0,
+    0, 0, 0, 0,
+    0, 0, 0, 0};
+
+bool ref[4][4]{false};
+
+void clear_ref()
+{
+    for (int i = 0; i < 4; i++)
+        for (int j = 0; j < 4; j++)
+            ref[i][j] = false;
+}
+
+void ref_cpy(bool block[][4])
+{
+    for (int i = 0; i < 4; i++)
+        for (int j = 0; j < 4; j++)
+            ref[i][j] = block[i][j];
+}
+//int find(int p, int n /*row*/) //return 在個row可以停下來
+//{                              //找哪邊可以放
+    //while ()
+    //{
+    /* code */
+    //}
+    /*for (int i = m-1; i >=0; i--)
+    {
+        for (int j = 0; j < n; j++
+        )
+            //cout << i * 10 + j << " ";
+            cout << *(table+i * n + j )<< " ";
+    }*/
+//}
 
 int main(int argc, char *argv[])
 {
     ifstream file(argv[1], ios::in);
     int m /*row*/, n /*column*/;
     file >> m >> n;
-    /*cout << "row:" << m << endl
-         << "column:" << n << endl;*/
 
-    bool *table = new bool[m * n]; //declare dynamic 2D arr
+    /*bool *table = new bool[m * n]; //declare dynamic 2D arr
     for (int i = 0; i < m; i++)
         for (int j = 0; j < n; j++)
-            *(table + i * n + j) = false;
+            *(table + i * n + j) = false;*/
 
-    /*for (int i = 0; i < m; i++)
+    /*for (int i = 0; i < m; i++) //print array
     {
         for (int j = 0; j < n; j++)
             cout << *(table + i * n + j) << " ";
         cout << endl;
     }*/
 
-    /*for (int i = m-1; i >=0; i--)
+    /*for (int i = m-1; i >=0; i--) //re print array
     {
         for (int j = 0; j < n; j++)
             //cout << i * 10 + j << " ";
@@ -137,14 +183,21 @@ int main(int argc, char *argv[])
             v = split(line);
             point = stoi(v[1]);
             move = stoi(v[2]);
-            //cout << v[0] << " " << point << " " << move << endl;
-            if (v[0].compare("O")==0)
-                cout<<"test";
+            clear_ref();
 
-
-
-                
-
+            if (v[0].compare("I1") == 0)
+            {
+                ref_cpy(T1_arr);
+                for (int i = 3; i >= 0; i--)
+                {
+                    for (int j = 0; j < 4; j++)
+                        cout << ref[i][j] << ' ';
+                    cout << endl;
+                }
+            }
+            else if (v[0].compare("T2") == 0)
+            {
+            }
         }
     } while (!file.eof());
 
