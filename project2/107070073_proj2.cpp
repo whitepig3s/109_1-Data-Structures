1065cad559c25123b127e86efb031b6aa73dfdc2
whitepig3s <whitepig3s@gmail.com>
2020-11-15 11:40:38 +0800

    Node

--- project2/107070073_proj2.cpp
+++ project2/107070073_proj2.cpp
@@ -17,13 +17,34 @@ void print_table(int rows, int columns)
     }
 }
 
-int main(int argc, char *argv[])
+struct Node{
+    int node_n, node_m;
+    Node *left, *right;
+};
+
+class binary_tree{
+private:
+    Node *first;  
+public:
+    Node add_Node(int tmp_m,int tmp_n){
+        Node *tmp = new Node;
+        tmp->node_m = tmp_m;
+        tmp->node_n = tmp_n;
+        tmp->left = NULL;
+        tmp->right = NULL;
+        return *tmp;
+    }
+
+};
+
+main(int argc, char *argv[])
 {
     ifstream file(argv[1], ios::in);
     int m /*row*/, n /*column*/, B /*battery*/;
     file >> m >> n >> B;
     string line;
     file.get();
+    int Rn, Rm;
     for (int i = 0; i < m; i++)
     {
         char tmp;
@@ -31,11 +52,17 @@ int main(int argc, char *argv[])
         {
             file >> tmp;
             table[i][j] = tmp;
+            if (tmp == 'R')
+            {
+                Rm = i;
+                Rn = j;
+            }
         }
         file.get();
     }
     file.close();
-    print_table(m, n);
+
+    //print_table(m, n);
     ofstream ofile("107070073_proj2", ios::out);
 
     ofile.close();
