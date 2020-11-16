e7ba8ec7794689d1e6cbf9e19d16085645dd4780
whitepig3s <whitepig3s@gmail.com>
2020-11-17 03:25:04 +0800

    traversal 完成

--- project2/107070073_proj2.cpp
+++ project2/107070073_proj2.cpp
@@ -6,6 +6,8 @@ using namespace std;
 
 char table[1000][1000] = {0};
 
+int t_m, t_n;
+
 queue<struct Node> nodequeue;
 
 void print_table(int rows, int columns)
@@ -29,6 +31,8 @@ class binary_tree
 {
 private:
     Node root;
+    Node blackhole;
+    bool full;
 
 public:
     Node add_Node(int tmp_m, int tmp_n)
@@ -39,15 +43,19 @@ public:
         tmp->child = NULL;
         tmp->sibling = NULL;
         tmp->exist = 0;
-        if (table[tmp_m][tmp_n] == '0' || table[tmp_m][tmp_n] == 'R')
+        if (tmp_m < t_m && tmp_n < t_n && tmp_m >= 0 && tmp_n >= 0)
         {
-            tmp->exist = 1;
+            if (table[tmp_m][tmp_n] == '0')
+            {
+                tmp->exist = 1;
+            }
         }
         return *tmp;
     }
 
     Node add_child(int tmp_m, int tmp_n)
     {
+        full = 0;
         Node n[4];
         n[0] = add_Node(tmp_m + 1, tmp_n);
         n[1] = add_Node(tmp_m, tmp_n + 1);
@@ -57,9 +65,10 @@ public:
         int first = 4;
         for (int i = 0; i < 4; i++)
         {
-            if (n[i].exist == 1)
+            if (n[i].exist == 1 && table[n[i].node_m][n[i].node_n] == '0')
             {
                 nodequeue.push(n[i]);
+                cout << n[i].node_m << " " << n[i].node_n << " " << endl;
                 if (first == 4)
                 {
                     first = i;
@@ -72,27 +81,38 @@ public:
                 table[n[i].node_m][n[i].node_n] = '2';
             }
         }
-        cout <<tmp_m<<" "<<tmp_n<<" "<<nodequeue.size() << endl;  //加了這行就可以運作正常
+        //cout <<tmp_m<<" "<<tmp_n<<" "<<nodequeue.size() << endl;  //加了這行就可以運作正常
+        cout<<"re"<<endl;
         if (first < 4)
             return n[first];
         else
-            return *(n[0].child); //return NULL
+        {
+            cout<<"test"<<endl;
+            full = 1;
+            //return *(n[0].child); //return NULL
+            return blackhole;
+        }
     }
 
     void build_tree(int rm, int rn)
     {
-        root = add_Node(rm, rn);
+        root.node_m = rm;
+        root.node_n = rn;
         Node c = add_child(rm, rn);
         root.child = &c;
-        //while (!nodequeue.empty())
-        do{
+        while (!nodequeue.empty())
+        {
             Node n = nodequeue.front();
-            Node c = add_child(n.node_m, n.node_n);
-            n.child = &c;
+            cout<<"fornt "<<n.node_m<<" "<<n.node_n<<endl;
+            c = add_child(n.node_m, n.node_n);
+             cout<<"test2"<<endl;
+            if (full == 0)
+            {
+                //c = add_child(n.node_m, n.node_n);
+                n.child = &c;
+            }
             nodequeue.pop();
-        }while (nodequeue.size()!=0);
-        //cout << (root.child)->node_m << " " << (root.child)->node_n<< endl;
-        //cout << node->node_m << " " << node->node_n<<"->  "<<node->cnum << endl;
+        }
     }
 
     void print(void)
@@ -115,6 +135,8 @@ main(int argc, char *argv[])
     ifstream file(argv[1], ios::in);
     int m /*row*/, n /*column*/, B /*battery*/;
     file >> m >> n >> B;
+    t_m = m;
+    t_n = n;
     string line;
     file.get();
     int Rm, Rn;
